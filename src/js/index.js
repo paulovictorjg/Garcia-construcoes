e5ca99b929da45bffffae49068ee0486127c2353
Paulo Victor <paulovictor1942@gmail.com>
2023-04-17 00:46:00 -0300

    slide auto

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,6 @@
-/* 
-	OBJETIVO - quando clicarmos no botão temos que mostrar a imagem de fundo correspondente
-
-    - passo 1 - dar um jeito de pegar o elemento HTML dos botões
-
-	- passo 2 - dar um jeito de identificar o clique do usuário no botão
-
-	- passo 3 - desmarcar o botão selecionado anterior
-
-	- passo 4 - marcar o botão clicado como se estivesse selecionada
-
-	- passo 5 - esconder a imagem de fundo anterior
-
-	- passo 6 - fazer aparecer a imagem de fundo correspondente ao botão clicado
-*/
-
-// passo 1 - dar um jeito de pegar o elemento HTML dos botões
 const botoesCarrossel = document.querySelectorAll('.botao');
 const imagens = document.querySelectorAll('.imagem');
 
-// passo 2 - dar um jeito de identificar o clique do usuário no botão
 botoesCarrossel.forEach((botao, indice) => {
     botao.addEventListener('click', () => {    
         desativarBotaoSelecionado();
@@ -48,3 +30,18 @@ function desativarBotaoSelecionado() {
     const botaoSelecionado = document.querySelector('.selecionado');
     botaoSelecionado.classList.remove('selecionado');
 }
+
+function percorrerSlide() {
+    const imagens = document.querySelectorAll('.imagem');
+    let indice = 0;
+    setInterval(() => {
+        esconderImagemAtiva();
+        imagens[indice].classList.add('ativa');
+        indice++;
+        if (indice === imagens.length) {
+            indice = 0;
+        }
+    }, 3000);
+}
+
+percorrerSlide();
