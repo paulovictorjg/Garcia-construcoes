22b13af86a5fb3b7c4537532bcdd50819339dcd1
Paulo Victor <89660799+paulovictorjg@users.noreply.github.com>
2023-10-07 23:18:48 -0300

    fotos novas

--- src/js/index.js
+++ src/js/index.js
@@ -1,47 +1,25 @@
-const botoesCarrossel = document.querySelectorAll('.botao');
-const imagens = document.querySelectorAll('.imagem');
+// Seleciona todas as imagens na galeria
+const imagens = document.querySelectorAll('.galeria-imagem');
 
-botoesCarrossel.forEach((botao, indice) => {
-    botao.addEventListener('click', () => {    
-        desativarBotaoSelecionado();
+// Adiciona um ouvinte de evento a cada imagem
+imagens.forEach((imagem) => {
+  imagem.addEventListener('click', (evento) => {
+    // Quando a imagem é clicada, altera o estilo para preencher a tela
+    evento.target.style.position = 'fixed';
+    evento.target.style.width = '100%';
+    evento.target.style.height = '100%';
+    evento.target.style.objectFit = 'cover';
+    evento.target.style.zIndex = '999';
+  });
+});
 
-        selecionarBotaoCarrosel(botao);
-
-        esconderImagemAtiva();
-
-        mostrarImagemDeFundo(indice);
-    })
-})
-
-function mostrarImagemDeFundo(indice) {
-    imagens[indice].classList.add('ativa');
-}
-
-function selecionarBotaoCarrosel(botao) {
-    botao.classList.add('selecionado');
-}
-
-function esconderImagemAtiva() {
-    const imagemAtiva = document.querySelector('.ativa');
-    imagemAtiva.classList.remove('ativa');
-}
-
-function desativarBotaoSelecionado() {
-    const botaoSelecionado = document.querySelector('.selecionado');
-    botaoSelecionado.classList.remove('selecionado');
-}
-
-function percorrerSlide() {
-    const imagens = document.querySelectorAll('.imagem');
-    let indice = 0;
-    setInterval(() => {
-        esconderImagemAtiva();
-        imagens[indice].classList.add('ativa');
-        indice++;
-        if (indice === imagens.length) {
-            indice = 0;
-        }
-    }, 3000);
-}
-
-percorrerSlide();
+// Fecha a visualização em tela cheia ao clicar novamente na imagem
+document.body.addEventListener('click', (evento) => {
+  if (evento.target.style.position === 'fixed') {
+    evento.target.style.position = '';
+    evento.target.style.width = '';
+    evento.target.style.height = '';
+    evento.target.style.objectFit = '';
+    evento.target.style.zIndex = '';
+  }
+});
